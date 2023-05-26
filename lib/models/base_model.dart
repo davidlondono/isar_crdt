9f15d8d5ed00f958e1bf827cde044e4f054f54a9
David Alejandro Londoño Mejía <davidlondono9@gmail.com>
2023-05-26 14:43:35 -0500

    reformat

--- lib/models/base_model.dart
+++ lib/models/base_model.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: invalid_use_of_protected_member
 
-
 import 'package:isar/isar.dart';
 import 'package:isar_crdt/models/models.dart';
 import 'package:isar_crdt/operations/operations.dart';
